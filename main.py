959f793b035d0117da7141878e8df903f7c4fc18
markyavbasarov-png <mark.yavbasarov@mail.ru>
2026-01-28 19:09:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.filters import CommandStart
 import aiosqlite
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 
