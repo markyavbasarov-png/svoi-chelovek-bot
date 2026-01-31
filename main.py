76cc98b70a8f304583d182a9e0417074bc40770a
markyavbasarov-png <mark.yavbasarov@mail.ru>
2026-01-31 12:35:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -360,6 +360,10 @@ async def notify_match(u1: int, u2: int):
         async def main():
     await init_db()
     await dp.start_polling(bot)
+# ================= RUN =================
+async def main():
+    await init_db()
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
