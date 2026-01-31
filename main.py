24c119e25ebdb5c71907c75373fc81db3c67ba44
markyavbasarov-png <mark.yavbasarov@mail.ru>
2026-01-31 16:17:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -352,12 +352,13 @@ async def send_my_profile(user_id: int):
             (user_id,)
         )
         profile = await cur.fetchone()
-if profile:
-    await send_profile_card(
-        chat_id=user_id,
-        profile=profile,
-        reply_markup=my_profile_view_kb()
-    )
+
+    if profile:
+        await send_profile_card(
+            chat_id=user_id,
+            profile=profile,
+            kb=my_profile_view_kb()
+        )
 # ======================== BROWSE =================
 @dp.callback_query(F.data == "browse")
 async def browse(call: CallbackQuery, state: FSMContext):
